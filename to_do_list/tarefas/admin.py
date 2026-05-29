17d2e0d2dea1de0f828385803b5739e14906e41e
Lobato2310 <lucaslobsouza@gmail.com>
2026-05-29 14:56:35 -0300

    remove arquivos ignorados do repositorio

--- to_do_list/tarefas/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Tarefa
-from simple_history.admin import SimpleHistoryAdmin
-
-class TarefaAdmin(SimpleHistoryAdmin):
-    list_display = ('titulo', 'status', 'prioridade', 'prazo', 'usuario', 'criado_em')
-admin.site.register(Tarefa, TarefaAdmin)
